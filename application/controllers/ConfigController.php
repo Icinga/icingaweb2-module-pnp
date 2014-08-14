c498d31f7f38c70ab3ed0872271d8d9d0715d32b
Thomas Gelf <thomas@gelf.net>
2014-08-14 18:53:57 +0200

    Improve config hint readabilty

--- application/controllers/ConfigController.php
+++ application/controllers/ConfigController.php
@@ -7,14 +7,17 @@ class Pnp4nagios_ConfigController extends ModuleActionController
     public function indexAction()
     {
         $this->view->tabs = $this->Module()->getConfigTabs()->activate('config');
-        $this->view->hint = sprintf($this->translate(
+        $hintHtml = $this->view->escape($this->translate(
             'Configuration form is still missing in this prototype.'
           . ' In case your PNP4Nagios config path is not %s or your base'
           . ' PNP4Nagios web url differs from %s please create a config file'
-          . ' in %s following this example:'),
-            '/etc/pnp4nagios',
-            '/pnp4nagios',
-            $this->Config()->getConfigFile()
+          . ' in %s following this example:'
+        ));
+        $this->view->escapedHint = sprintf(
+            $hintHtml,
+            '<b>/etc/pnp4nagios</b>',
+            '<b>/pnp4nagios</b>',
+            '<b>' . $this->Config()->getConfigFile() . '</b>'
         );
     }
 }
